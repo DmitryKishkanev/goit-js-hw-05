5618c5e4f7e25d947c593a2fe830c2e8c20c2996
Dmitry Kishkanev <150253586+DmitryKishkanev@users.noreply.github.com>
2025-01-22 19:04:36 +0200

    готово

--- js/task-1.js
+++ js/task-1.js
@@ -1,41 +1,43 @@
-const filter = function (array, test) {
-  const filteredArray = [];
-
-  for (const el of array) {
-    console.log(el);
-    const passed = test(el);
-
-    if (passed) {
-      filteredArray.push(el);
-    }
-  }
-
-  return filteredArray;
+const getUserNames = (users) => {
+  return users.map((user) => user.name);
 };
 
-const clollback1 = function (value) {
-  return value >= 3;
-};
-
-const clollback2 = function (value) {
-  return value <= 4;
-};
-
-const r1 = filter([1, 2, 3, 4, 5], clollback1);
-console.log(r1);
-
-const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], clollback2);
-console.log(r2);
-
-const fruits = [
-  { name: "aples", quantity: 200, isFresh: true },
-  { name: "grapes", quantity: 150, isFresh: false },
-  { name: "bananas", quantity: 100, isFresh: true },
-];
-
-const getFruitsWithQuantity = function (fruit) {
-  return fruit.quantity >= 120;
-};
-
-const r3 = filter(fruits, getFruitsWithQuantity);
-console.log(r3);
+console.log(
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811,
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821,
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793,
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278,
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951,
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498,
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764,
+    },
+  ])
+);
